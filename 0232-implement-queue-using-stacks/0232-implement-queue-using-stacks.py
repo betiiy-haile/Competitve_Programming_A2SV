8f5a132a95b199b5de8a04e947a3c4b44eae6a2e
betiiy-haile <108091111+betiiy-haile@users.noreply.github.com>
2022-12-28 14:41:10 +0300

    Time: 24 ms (96.71%), Space: 13.9 MB (68.35%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,36 +1,26 @@
 class MyQueue:
 
     def __init__(self):
-        self.st1=[]
-        self.st2=[]
+        self.queue = []
+        self.p = []
 
-    def push(self, x):
-        self.st1.append(x)
+    def push(self, x: int) -> None:
+        while self.queue:
+            self.p.append(self.queue.pop())
+        self.queue.append(x)
+        while self.p:
+            self.queue.append(self.p.pop())
 
-    def pop(self):
-        while self.st1:
-            self.st2.append( self.st1.pop() )
+    def pop(self) -> int:
+        return self.queue.pop()
 
-        x=self.st2.pop()
+    def peek(self) -> int:
+        return self.queue[-1]
 
-        while self.st2:
-            self.st1.append( self.st2.pop() )
-        return x
+    def empty(self) -> bool:
+        return len(self.queue) == 0
 
-    def peek(self):
-        while self.st1:
-            self.st2.append(self.st1.pop())
-        x=self.st2[-1]
-
-        while self.st2:
-            self.st1.append( self.st2.pop() )
-        return x
-
-    def empty(self):
-        return not (self.st1 or self.st2)
-
-
-
+    
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
