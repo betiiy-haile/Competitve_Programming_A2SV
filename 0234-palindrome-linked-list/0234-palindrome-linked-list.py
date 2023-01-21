8e39cd667fdc68ca713dfc805ae27ebbcf22320d
betiiy-haile <108091111+betiiy-haile@users.noreply.github.com>
2023-01-21 12:39:14 +0300

    Time: 760 ms (89.54%), Space: 39 MB (86.10%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,16 +5,40 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        curr = head
-        arr = []        
-        while curr != None:
-            arr.append(curr.val)
-            curr =  curr.next   
+#         curr = head
+#         arr = []        
+#         while curr != None:
+#             arr.append(curr.val)
+#             curr =  curr.next               
+#         return arr == arr[::-1]:
+
+        fast = head
+        slow = head
+        
+        # Use fast slow pointers to find the middle node of the list
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Reverse the second half of the linked list
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
             
-        if arr == arr[::-1]:
-            return True
-        else: 
-            return False
+        # Check palindrome with two pointers
+        left = head
+        right = prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
+            
                 
             
             
