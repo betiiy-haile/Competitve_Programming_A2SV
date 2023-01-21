70e47b2032c0a2620bcf2a89dcc108afac49ce27
betiiy-haile <108091111+betiiy-haile@users.noreply.github.com>
2023-01-21 15:19:40 +0300

    Time: 98 ms (86.73%), Space: 15.2 MB (68.13%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,13 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            while stack and stack[-1] >= 0 and a < 0: # collision happens
+                collision = stack[-1] + a # collision result
+                if collision <= 0: 
+                    stack.pop() # stack top is destroyed
+                if collision >= 0: 
+                    break # new astroid is destroyed
+            else:
+                stack.append(a)
+        return stack
