9db613633528a1e1df6fa4f8a3a0814acaacf4e0
betiiy-haile <108091111+betiiy-haile@users.noreply.github.com>
2023-03-02 09:13:19 +0300

    Time: 983 ms (48.88%), Space: 20.4 MB (29.03%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,28 +1,17 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        first = -1
-        second = -1
-        secondCount = 0
-        currMax = 0
-        Max = 0
-        
-        for fruit in fruits:
-            if fruit == first or fruit == second:
-                currMax += 1
-            else:
-                currMax = secondCount + 1
-        
-            if fruit == second:
-                secondCount += 1
-            else:
-                secondCount = 1
+        d = defaultdict(int)
+        maxLen = 0
+        left = 0
+        for right in range(len(fruits)):
+            d[fruits[right]] += 1            
+            while len(d) > 2:
+                d[fruits[left]] -= 1
+                if d[fruits[left]] == 0:
+                    d.pop(fruits[left])
+                left += 1
+            maxLen = max(maxLen , right-left+1)
                 
-            if(fruit != second):
-                first = second
-                second = fruit
-                
-            Max = max(Max,currMax)
-        return Max
-            
-
-        
+        return maxLen
+    
+    
