216ac0baca15b013de731cd61910a30a3c1e6b71
Betelhem Haile <108091111+betiiy-haile@users.noreply.github.com>
2023-10-02 16:48:03 +0300

    added best-team-with-no-conflicts.python

---
+++ best-team-with-no-conflicts.py
@@ -0,0 +1,15 @@
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        pairs = sorted(zip(scores, ages))
+        pairs.sort()
+
+        dp = [pairs[i][0] for i in range(len(pairs))]
+        for i in range(len(pairs)):
+            mScore, mAge = pairs[i]
+
+            for j in range(i):
+                score, age = pairs[j]
+                if mAge >= age:
+                    dp[i] = max(dp[i], mScore + dp[j])
+            
+        return max(dp)
