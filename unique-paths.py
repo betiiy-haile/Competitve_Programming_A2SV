d66491f5683b47cb55c4ba9135a669c0630543b3
Betelhem Haile <108091111+betiiy-haile@users.noreply.github.com>
2023-10-16 16:49:39 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,19 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-
-        def inBound(row, col):
-            return 0 <= row < m and 0 <= col < n
-
-        def dp(row, col):
-            if row == m - 1 and col == n -1:
-                return 1
-            ans = 0
-            if (row, col) not in memo:
-                for (r, c) in [(0,1), (1,0)]:
-                    if inBound(row + r, col + c):
-                        ans += dp(row + r, col + c)   
-                memo[(row, col)] = ans
-            return memo[(row, col)]
-
-        return dp(0, 0)
+        
+        def factorial(n):
+            ans = 1
+            while n > 1:
+                ans *= n
+                n -= 1
+            return ans
+            
+        val = factorial(n + m - 2) 
+        val2 = factorial(m - 1) * factorial(n - 1)
+        return val // val2
