0420a586f9598fe66df84dcabf4e8cbb07b486df
Betelhem Haile <108091111+betiiy-haile@users.noreply.github.com>
2024-05-03 08:35:38 +0300

    Time: 37 ms (80.72%), Space: 17.1 MB (9.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+
+        n = len(citations)
+        citations.sort()
+        for i in range(len(citations)):
+            if n - i <= citations[i]:
+                return n - i
+
+        return 0
+
